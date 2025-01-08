1ed8c739d784d1f88648b07e3736ff2bdc9399e4
iamhabee <habeehola18@gmail.com>
2025-01-08 17:41:35 +0100

    authentication module completed

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -1,37 +1,87 @@
 import { Request, Response } from "express";
 import authService from "../../services/auth.service";
-import { Prisma } from "@prisma/client";
 
 
-
-const postLogIn = async (req: Request, res: Response ): Promise<any> =>{
+const AuthController = {
+  postLogIn: async (req: Request, res: Response ): Promise<any> =>{
     try{
-
-        const response = await authService.postLogIn(req);
-        if(!response.status){
-            return res.status(400).json(response);
-        }
-
-        res.json(response)
+      const response = await authService.postLogIn(req);
+      if(!response.status){
+        return res.status(400).json(response);
+      }
+      res.json(response)
     }catch(err){
-        console.log(err);
-            // if (err?.message ) {
-            //     if (err.code === 'P2002') {
-            //       return res.status(400).json({
-            //         message: 'A user with this email already exists.',
-            //         field: (err.meta?.target as string[]).join(", "),
-            //         status: false,
-            //       });
-            //     }
-            //   }
-            res.status(500).json({
-                message: `Internal Server Error`,
-                status: false,
-            });
-        }
+      console.log(err);
+      res.status(500).json({
+        message: `Internal Server Error`,
+        status: false,
+      });
+    }
+  },
+
+  postCreateUser: async (req : Request, res: Response): Promise<any>=>{
+    try{
+      const response = await authService.postCreateUser(req)
+
+      if(!response.status){
+        return res.status(400).json(response);
+      }
+      res.status(200).json(response);
+
+    }catch(err){
+      res.status(500).json({
+        message: `Internal Server Error`,
+        status: false,
+      });
+    }
+  },
+
+  forgotPassword: async (req : Request, res: Response): Promise<any>=>{
+    try{
+      const response = await authService.forgotPassword(req)
+
+      if(!response.status){
+        return res.status(400).json(response);
+      }
+      res.status(200).json(response);
+
+    }catch(err){
+      res.status(500).json({
+        message: `Internal Server Error`,
+        status: false,
+      });
+    }
+  },
+
+  verifyToken: async (req : Request, res: Response): Promise<any>=>{
+    try{
+      const response = await authService.verifyToken(req)
+      if(!response.status){
+        return res.status(400).json(response);
+      }
+      res.status(200).json(response);
+    }catch(err){
+      res.status(500).json({
+        message: `Internal Server Error`,
+        status: false,
+      });
+    }
+  },
+  
+  resetPassword: async (req : Request, res: Response): Promise<any>=>{
+    try{
+      const response = await authService.resetPassword(req)
+      if(!response.status){
+        return res.status(400).json(response);
+      }
+      res.status(200).json(response);
+    }catch(err){
+      res.status(500).json({
+        message: `Internal Server Error`,
+        status: false,
+      });
+    }
+  }
 }
 
-
-export {
-    postLogIn
-}
+export default AuthController
--- src/services/index.service.ts
+++
@@ -1,9 +0,0 @@
-import userService from "./user.service";
-import authService from "./user.service";
-
-
-
-export {
-    userService,
-    authService
-}
